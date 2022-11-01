dee4522240c40da3b98feff12e7958e29552e9be
lucnv <luc.nguyen@edutalk.edu.vn>
2022-11-02 01:36:36 +0700

    add body request parser

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extened: true
+}));
+app.use(express.json());
 //http logger
 const morgan = require('morgan');
 app.use(morgan('combined'));
@@ -21,6 +27,10 @@ app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/search', (req, res) => {
+  res.render('search')
 })
 
 app.listen(port, () => {
