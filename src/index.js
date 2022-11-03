364374e332066f8a23a5f6d7e5d0bc2733034239
lucnv <luc.nguyen@edutalk.edu.vn>
2022-11-03 23:41:54 +0700

    add prettier

--- src/index.js
+++ src/index.js
@@ -8,29 +8,31 @@ const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.urlencoded({
-  extened: true
-}));
+app.use(
+  express.urlencoded({
+    extened: true,
+  }),
+);
 app.use(express.json());
 //http logger
 const morgan = require('morgan');
 app.use(morgan('combined'));
 
 //template engine
-const exphbs = require("express-handlebars");
-const { engine } = require("express-handlebars");
-app.engine("hbs", engine(
-  {
-    extname: ".hbs"
-  }
-));
+const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //route init
 route(app);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
