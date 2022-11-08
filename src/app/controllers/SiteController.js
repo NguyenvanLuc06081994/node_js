fe71e870435f60c781e16610c21837a3c3dfa5d7
lucnv <luc.nguyen@edutalk.edu.vn>
2022-11-09 01:22:31 +0700

    read data from database and render homepage

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,16 @@
+const { render } = require('node-sass');
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     index(req, res, next) {
-        Course.find({}, function (err, courses) {
-            if (!err) {
-                res.json(courses);
-                return;
-            }
-            res.status(400).json({ error: 'ERROR' });
-        });
-        // res.render('home');
+        Course.find({})
+            .then((courses) => {
+                res.render('home', {
+                    courses: courses.map((course) => course.toObject()),
+                });
+            })
+            .catch(next);
     }
 
     search(req, res) {
